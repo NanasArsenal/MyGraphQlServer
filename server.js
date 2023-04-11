fade86464682d3d56bf8946b395d00ba4077ec4a
Nana Kweku <nanakwekudev@gmail.com>
2023-04-11 15:31:09 +0000

    first run

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {buildSchema} = require('graphql');
+const {graphqlHTTP}  = require('express-graphql');
+
+const app = express()
+
+
+const schema = buildSchema(`
+    type Query{
+        hello:String
+    }
+`);
+
+
+const root = {
+    hello: ()=>{
+        return "Hello World"
+    }
+}
+
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema:schema,
+    rootValue: root
+}))
+
+
+
+app.listen(4000 , ()=>{
+    console.log(`Server running on 4000`)
+})
