f11f6cb252c4035da66452471ab8fe80b280f568
Nana Kweku <nanakwekudev@gmail.com>
2023-04-12 15:56:25 +0000

    still learning

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const {buildSchema} = require('graphql');
 const {graphqlHTTP}  = require('express-graphql');
-
+const axios = require('axios')
 const app = express()
+
+let message = "I love you"
 
 
 const schema = buildSchema(`
+    type User{
+        name:String,
+        age: Int,
+        college:String,
+    }
+    type Post{
+        userId: Int,
+        id    : Int,
+        title : String,
+        body  : String,
+    },
     type Query{
-        hello:String
+        hello:String,
+        numerals: [Int],
+        welcomeMessage(name: String, dayOfWeek:String! ): String,
+        getUser: User,
+        getUsers: [User],
+        getPosts: [Post],
+        message : String,
+    },
+
+    type Mutation{
+        setMessage(newMessage: String): String,
+        createUser(name:String,age:Int,college:String): User
     }
 `);
 
@@ -15,7 +39,55 @@ const schema = buildSchema(`
 const root = {
     hello: ()=>{
         return "Hello World"
-    }
+    },
+    numerals: ()=>{
+        return [1,2,3,4,5,]
+    },
+    welcomeMessage: (args)=>{
+        return `Hello ${args.name} it is  ${args.dayOfWeek}`;
+    },
+    getUser : ()=>{
+        let user = {
+            name:"Peter",
+            age:18,
+            college:"UG"
+        }
+        return user
+    },
+    getUsers : ()=>{
+        let users =[
+            {
+                name:"Peter",
+                age:18,
+                college:"UG"
+            },    {
+                name:"JOHN",
+                age:18,
+                college:"UG"
+            },
+            {
+                name:"Jeremy",
+                age:18,
+                college:"Ucla"
+            },
+        ]
+        return users
+    },
+    getPosts : async ()=>{
+        return await axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(function (response) {
+            return response.data;
+          })
+          .catch(function(error){
+            console.log(error)
+          })
+    },
+    setMessage : function(args){
+        message = args.newMessage ;
+        return message;
+    },
+    message: ()=> message,
+
 }
 
 
